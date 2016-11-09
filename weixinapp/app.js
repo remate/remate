9c68c77b3b1b76e6d9701ce1493c05ad65c27255
zuoci <452642848@qq.com>
2016-11-09 17:10:51 +0800

    a

---
+++ weixinapp/app.js
@@ -0,0 +1,38 @@
+//app.js
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    console.log("初始化完成")
+  },
+  onHide:function(){
+    console.log("进入后台")
+  },
+  onShow:function(){
+    console.log("恢复前台工作")
+  },
+  getUserInfo:function(cb){
+    var that = this;
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo;
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      });
+    }
+  },
+  globalData:{
+    userName:"zuoci",
+    userInfo:null
+  }
+})
