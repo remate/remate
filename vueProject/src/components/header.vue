e95bc2dccd197119c57fc2ed589ca848df1622d2
zuoci <zuoci@ixiaopu.com>
2017-03-27 14:02:38 +0800

    a

--- vueProject/src/components/header.vue
+++ vueProject/src/components/header.vue
@@ -17,17 +17,33 @@
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
             </div>
-            <div v-if="seller.supports" class="support-count">
+            <div v-if="seller.supports" class="support-count" @click="showDetail">
                 <span class="count">{{seller.supports.length}}个</span>
                 <span class="icon-circle-right"></span>
             </div>
         </div>
-        <div class="bulletin-wrapper">
+        <div class="bulletin-wrapper" @click="showDetail">
             <span class="bulletin-title">公告</span><span class="bulletin-text">{{seller.bulletin}}</span>
             <span class="icon-circle-right"></span>
         </div>
         <div class="background">
             <img :src="seller.avatarPath" width="100%" height="100%">
+        </div>
+        <div class="detail" v-show="detailShow">
+            <div class="detail-wrapper">
+                <div class="detail-main">
+                    <div class="name">{{seller.name}}</div>
+                    <v-star :score="seller.score"></v-star>
+                </div>
+                <div class="title">
+                    <div class="line"></div>
+                    <div class="text"></div>
+                    <div class="line"></div>
+                </div>
+            </div>
+            <div class="detail-close" @click="hideDetail">
+                <i class="close">X</i>
+            </div>
         </div>
     </div>
 </template>
@@ -35,13 +51,22 @@
 <script type="text/ecmascript-6">
     // eslint-disable-next-line
     /* eslint-disable */
+    import star from './star.vue';
     export default {
-       /*props: ['seller'],
+       /*props: ['seller'],*/
         data() {
             return {
-                seller: this.seller
+                detailShow:false
             }
-        }   方法一不采用对象模式*/
+        },   //方法一不采用对象模式
+        methods:{
+            showDetail() {
+                this.detailShow = true;
+            },
+            hideDetail() {
+                this.detailShow = false;
+            }
+        },
        props:{
            seller:{
                type:Object
@@ -49,8 +74,11 @@
        },   //方法一采用对象模式  type为传入输入的验证类型
         filters:{
            deliveryTime:function(time){
-               return time+"分钟送达";
+               return time + "分钟送达";
            }
+        },
+        components:{
+            'v-star':star
         }
     };
 </script>
@@ -58,14 +86,13 @@
 <style lang="stylus" rel="stylesheet/stylus">
     @font-face
         font-family: 'icomoon'
-        src:  url('/static/fonts/icomoon.eot?lndju3')
-        src:  url('/static/fonts/icomoon.eot?lndju3#iefix') format('embedded-opentype'),
-                url('/static/fonts/icomoon.ttf?lndju3') format('truetype'),
-                url('/static/fonts/icomoon.woff?lndju3') format('woff'),
-                url('/static/fonts/icomoon.svg?lndju3#icomoon') format('svg');
+        src:  url('/static/fonts/icomoon.eot?myorls');
+        src:  url('/static/fonts/icomoon.eot?myorls#iefix') format('embedded-opentype'),
+            url('/static/fonts/icomoon.ttf?myorls') format('truetype'),
+            url('/static/fonts/icomoon.woff?myorls') format('woff'),
+            url('/static/fonts/icomoon.svg?myorls#icomoon') format('svg')
         font-weight: normal
         font-style: normal
-
 
     [class^="icon-"], [class*=" icon-"]
         /* use !important to prevent issues with browser extensions that change fonts */
@@ -83,6 +110,7 @@
     .header
         color #fff
         position relative
+        overflow hidden
         background rgba(7,17,27,.5)
         /*background #AB9795*/
         .content-wrapper
@@ -165,7 +193,7 @@
                 background #fff
                 color: #000
                 border-radius 2px
-                margin-top 7px
+                margin-top 8px
             .bulletin-text
                 vertical-align top
                 font-size 12px
@@ -187,5 +215,47 @@
             height 100%
             z-index -1
             filter blur(10px)
-
+        .detail
+            position fixed
+            top 0
+            left 0
+            z-index 100
+            width 100%
+            height 100%
+            overflow auto
+            background rgba(1,17,27,.8)
+            .detail-wrapper
+                min-height 100%
+                overflow hidden
+                .detail-main
+                    margin-top 64px
+                    padding-bottom 64px
+                    .name
+                        line-height 16px
+                        text-align center
+                        font-size 16px
+                        font-weight 700
+                    .title
+                        display flex
+                        width:80%
+                        margin:30px auto 24px auto
+                        .line
+                            flex 1
+                            position relative
+                            top: -6px
+                            border-bottom 1px solid rgba(255,255,255,.2)
+                        .text
+                            padding: 0 12px
+                            font-size 14px
+            .detail-close
+                position relative
+                width 32px
+                height 32px
+                line-height 32px
+                margin -50px auto 0 auto
+                clear: both
+                font-size 20px
+                text-align center
+                border-radius 16px
+                border:1px solid #fff
 </style>
